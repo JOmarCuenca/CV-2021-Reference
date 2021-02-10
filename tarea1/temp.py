c6fcf62d0b3bef2b1af8303dbed91e69be249165
Jesus Omar <jesomar.cuenca@gmail.com>
2021-02-10 16:48:11 -0600

    Tarea 1 terminada

--- tarea1/temp.py
+++ tarea1/temp.py
@@ -1,1 +1,13 @@
-import numpy
+import numpy as np
+
+def null_matrix(m,n):
+    return [[0]*n]*m
+
+print(null_matrix(3,4))
+
+print([[['?']*2]*3]*4)
+
+m = {'Disney+': 7, 'Netflix': 8.99, 'Prime': 12.99}
+
+
+raw = np.array(range(1,17))
