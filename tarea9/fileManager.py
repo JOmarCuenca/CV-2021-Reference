b407449c286bbcca8a89df89143434986f95bd51
Jesus Omar <jesomar.cuenca@gmail.com>
2021-03-25 17:03:06 -0600

    Create, Save & Delete Folders on command

---
+++ tarea9/fileManager.py
@@ -0,0 +1,22 @@
+import os
+
+def createFolder(dirName : str):
+    try:
+        os.mkdir(dirName)
+    except FileExistsError:
+        print(f"Folder {dirName} already Exists")
+
+def deleteFolder(dirName : str):
+    try:
+        os.rmdir(dirName)
+    except Exception:
+        print(f"Folder doesn't exist")
+
+if __name__ == "__main__":
+    import time
+    TEST_DIR = "ExampleDir"
+    createFolder(TEST_DIR)
+    print("Folder Created")
+    time.sleep(1)
+    deleteFolder(TEST_DIR)
+    print("Folder Deleted")
--- tarea9/imgScrapperTest.py
+++ tarea9/imgScrapperTest.py
@@ -6,22 +6,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 import sys
 import time
+from fileManager import createFolder,deleteFolder
 
 WEBSITE_URL = "https://unsplash.com"
-
-driver = Chrome("/home/omar/.selenium/chromedriver")
-
-driver.get(WEBSITE_URL)
-
-searchBar = driver.find_element_by_name("searchKeyword")
-
-searchBar.send_keys(sys.argv[1])
-searchBar.send_keys(Keys.ENTER)
-
-# Get scroll height
-last_height = driver.execute_script("return document.body.scrollHeight")
-new_height  = -1
-
 imgURL = []
 
 def getURLs(images):
@@ -35,48 +22,84 @@ def cleanDuplicates():
     global imgURL
     imgURL = list(dict.fromkeys(imgURL))
 
-iters = 0
-toBottom = False
+def writeImgs(urls : list):
+    targetFolder = "train/"
+    for x in range(len(urls)):
+        if(x > len(urls) * 4/5):
+            targetFolder = "test/"
+        webImg = requests.get(urls[x])
+        f = open(f"./{targetFolder}img_{x}.png","wb")
+        f.write(webImg.content)
+        f.close()
 
-while len(imgURL) < 150 and iters < 20:
+def mainActivity(driver, currentHeight, targetNumImgs):
+    global imgURL
+    # Get scroll height
+    last_height = currentHeight
+    new_height  = -1
+    iters = 0
+    toBottom = False
+    while len(imgURL) < targetNumImgs and iters < 20:
+        # Wait to load page
+        time.sleep(1)
 
-    # Wait to load page
-    time.sleep(1)
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight*31/33);" if toBottom else "window.scrollTo(0, document.body.scrollHeight*9/11);")
 
-    # Scroll down to bottom
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight*31/33);" if toBottom else "window.scrollTo(0, document.body.scrollHeight*9/11);")
+        # Wait to load page
+        time.sleep(2)
 
-    # Wait to load page
-    time.sleep(2)
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
 
-    # Calculate new scroll height and compare with last scroll height
-    new_height = driver.execute_script("return document.body.scrollHeight")
+        images = driver.find_elements_by_class_name("_2UpQX")
 
-    images = driver.find_elements_by_class_name("_2UpQX")
+        getURLs(images)
+        cleanDuplicates()
 
-    getURLs(images)
-    cleanDuplicates()
+        iters += 1
 
-    print(len(imgURL))
+        if new_height == last_height:
+            print("Height didn't update")
+            if(toBottom):
+                break
+            else:
+                toBottom = True
+        last_height = new_height
 
-    iters += 1
 
-    if new_height == last_height:
-        print("Height didn't update")
-        if(toBottom):
-            break
-        else:
-            toBottom = True
-    last_height = new_height
+if __name__ == "__main__":
+    driver = Chrome("/home/omar/.selenium/chromedriver")
+    driver.get(WEBSITE_URL)
+    searchBar = driver.find_element_by_name("searchKeyword")
 
-driver.close()
+    openBrowser = True
 
-targetFolder = "train/"
+    try:
+        searchBar.send_keys(sys.argv[1])
+        searchBar.send_keys(Keys.ENTER)
+    except IndexError:
+        openBrowser = False
+        driver.close()
 
-for x in range(len(imgURL)):
-    if(x > len(imgURL) * 4/5):
-        targetFolder = "test/"
-    webImg = requests.get(imgURL[x])
-    f = open(f"./{targetFolder}img_{x}.png","wb")
-    f.write(webImg.content)
-    f.close()
+    targetNumImgs = -1
+
+    try:
+        targetNumImgs = int(sys.argv[2])
+    except IndexError:
+        targetNumImgs = 100
+
+    # Get scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    if(openBrowser):
+        mainActivity(driver, last_height, targetNumImgs)
+
+        driver.close()
+
+        deleteFolder("train")
+        createFolder("train")
+        deleteFolder("test")
+        createFolder("test")
+
+        writeImgs(imgURL)
